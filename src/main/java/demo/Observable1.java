77707a6f0b034ac90fecf09d53c9a7fc223f35b5
vevhack <vevhack@gmail.com>
2015-08-24 02:04:04 +0800

    RX

---
+++ src/main/java/demo/Observable1.java
@@ -0,0 +1,39 @@
+package demo;
+
+import rx.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Zen on 2015/8/24.
+ */
+public class Observable1 {
+    /*Creating an Observable from an Existing Data Structure*/
+    public static void main(String[] args) {
+        Observable<Object> o = Observable.just("1", "2", "3");
+        o.subscribe(s -> System.out.println(s));
+        List<Integer> lists = new ArrayList<>();
+        lists.add(1111);
+        lists.add(2222);
+        lists.add(3333);
+        lists.add(4444);
+        Observable.from(lists).subscribe(s -> System.out.println(s));
+    }
+
+    /*通过create方法来创建一个Observable对象*/
+//    同步例子
+    public static Observable customObservableBlocking() {
+
+        return Observable.create(aSubscriber -> {
+            for (int i = 0; i < 50; i++) {
+                if (aSubscriber.isUnsubscribed()) {
+                    aSubscriber.onNext("value" + i);
+                }
+            }
+            if (aSubscriber.isUnsubscribed()) {
+                aSubscriber.onCompleted();
+            }
+        });
+    }
+}
--- src/main/java/demo/Operators.java
+++ src/main/java/demo/Operators.java
@@ -1,12 +1,48 @@
 package demo;
 
 import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by Zen on 2015/8/23.
  */
 public class Operators {
     public static void main(String[] args) {
-        Observable.just("Hello World").map(String::hashCode).map(s -> Integer.toString(s)).subscribe(System.out::println);
+//        Observable.just("Hello World").map(String::hashCode).map(s -> Integer.toString(s)).subscribe(System.out::println);
+//        Integer[] numbers = {0, 1, 2, 3, 4, 5};
+//        Observable.from(numbers).subscribe(
+//                (incomingNumber) -> System.out.println("incomingNumber" + incomingNumber),
+//                (error) -> System.out.println("something went wrong" + error.getMessage()),
+//                () -> System.out.println("This observable is finished")
+//        );
+
+//        create();
+        helloWorld("Mathew", "Zen");
     }
+
+
+    public static void create() {
+        Observable.OnSubscribe<String> subscribeFunction = (s) -> {
+            Subscriber subscriber = (Subscriber) s;
+            for (int ii = 0; ii < 10; ii++) {
+                if (!subscriber.isUnsubscribed()) {
+                    subscriber.onNext("push value" + ii);
+                }
+            }
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onCompleted();
+            }
+        };
+        Observable createObservable = Observable.create(subscribeFunction);
+        createObservable.subscribe(
+                (incomingValue) -> System.out.println("" + incomingValue),
+                (error) -> System.out.println("something went wrong" + ((Throwable) error).getMessage()),
+                () -> System.out.println("this observable is finished")
+        );
+    }
+
+    public static void helloWorld(String... names) {
+        Observable.from(names).subscribe(s -> System.out.println("hello:" + s));
+    }
+
 }
