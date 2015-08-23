fc1d543b0f5d1879a854039eb3cd72f185b87a70
vevhack <vevhack@gmail.com>
2015-08-23 22:26:26 +0800

    RX

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -1,8 +1,13 @@
 package demo;
 
+
+import rx.Observable;
+
 /**
  * Created by Zen on 2015/8/23.
  */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        Observable.just("Hello World").subscribe(s -> System.out.println(s));
+    }
 }
