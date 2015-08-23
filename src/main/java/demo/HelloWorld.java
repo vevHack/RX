f8b5096a3ea04a2a0456c70d3dec23c5440de0ae
vevhack <vevhack@gmail.com>
2015-08-24 03:00:29 +0800

    RX

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -3,11 +3,22 @@ package demo;
 
 import rx.Observable;
 
+import java.util.Random;
+
 /**
  * Created by Zen on 2015/8/23.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        Observable.just("Hello World").subscribe(System.out::println);
+//        Observable.just("Hello World").subscribe(System.out::println);
+        cache();
+    }
+    public static void cache(){
+        Random random = new Random();
+        Observable<Integer> observable = Observable.range(1, 100).map(random::nextInt).cache();
+        observable.subscribe(System.out::println);
+        System.out.println("cache..................");
+        observable.subscribe(System.out::println);
+
     }
 }
